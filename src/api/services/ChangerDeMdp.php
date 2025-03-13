a4ab117fed40bf8962fa9a9152d8588aca8d23d5
Tom <tom.lefevrebonzon@gmail.com>
2025-03-13 11:50:19 +0100

    wip

--- src/api/services/ChangerDeMdp.php
+++ src/api/services/ChangerDeMdp.php
@@ -44,7 +44,7 @@ else {
         $code_reponse = 400;
     }
     else {
-        if ( strlen($nouveauMdp) < 3 ) {
+        if ( strlen($nouveauMdp) < 8 ) {
             $msg = 'Erreur : le mot de passe doit comporter au moins 8 caractères.';
             $code_reponse = 400;
         }
